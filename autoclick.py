710627b3e75bd81b49f493c50e682a258859cf31
Tommaso <tommaso.ceredi@studio.unibo.it>
2023-12-06 20:53:29 +0100

    Add autoclick functionality

---
+++ autoclick.py
@@ -0,0 +1,30 @@
+import time
+import pyautogui
+
+def get_mouse_coordinates():
+    while True:
+        input("Posiziona il mouse dove vuoi cliccare e premi invio")
+        x, y = pyautogui.position()
+        print(f"Coordinate x: {x}, y: {y}")
+        return x, y
+    
+def number_of_clicks():
+    while True:
+        try:
+            n = int(input("Inserisci il numero di click: "))
+            return n
+        except ValueError:
+            print("Inserisci un numero intero")
+            
+def click(x, y, n):
+    for i in range(n):
+        pyautogui.click(x, y)
+        time.sleep(1)
+        
+def main():
+    x, y = get_mouse_coordinates()
+    n = number_of_clicks()
+    click(x, y, n)
+    
+if __name__ == "__main__":
+    main()
